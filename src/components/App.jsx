230fbff1bf098585d9935d5d0a0836c96c083688
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-07-17 11:20:42 +0300

    refactoring / setPerPage removed, error handled, etc

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,45 +4,31 @@ import { Button } from './Buton/Button';
 import { SearchBar } from './SearchBar/SearchBar';
 import { getPicturesByQuery } from 'services/PixabayAPI';
 import { Loader } from './Loader/SkeletonImage';
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 
 export const App = () => {
   const [searchQuery, setSearchQuery] = useState(null);
   const prevSearchQueryRef = useRef(null);
   const [page, setPage] = useState(1);
-  const [perPage, setPerPage] = useState(12); // 'setPerPage' is declared but its value is never -_-
+  const [perPage] = useState(12);
   const [totalImages, setTotalImages] = useState(0);
   const [pictures, setPictures] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null); // 'error' is declared but its value is never read. -_-
-
-  // 1. React Hook useEffect has a missing dependency: 'getPictures'. Either include it or remove the dependency array.eslintreact-hooks/exhaustive-deps
-  // Ок, добавил getPictures в массив зависимостей
-  // 2. 'getPictures' was used before it was defined.eslintno-use-before-define
-  // Ок, вынес getPictures выше useEffect
-  // 3. The 'getPictures' function makes the dependencies of useEffect Hook (at line 52) change on every render. Move it inside the useEffect callback. Alternatively, wrap the definition of 'getPictures' in its own useCallback() Hook.eslintreact-hooks/exhaustive-deps
-  // Чертов линтер, из-за всех этих warning у меня github build крашит
-
-  // Нагуглил два решения:
-  // а) "scripts": {
-  //      "build": "CI=false && react-scripts build",  // Add CI=False here
-  // б) env:
-  //      CI: false;
-
-  // Compiled with warnings.
-  // Хз хорошо это или не очень
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    getPictures();
-    prevSearchQueryRef.current = searchQuery;
-  }, [page, searchQuery]);
+    if (!error) return;
+    toast.error(error);
+  }, [error]);
 
-  const getPictures = async () => {
+  const getPictures = useCallback(async () => {
     try {
       setIsLoading(true);
+      setError(null);
       const normalizedSearchQuery = searchQuery.toLowerCase().trim();
+      prevSearchQueryRef.current = normalizedSearchQuery;
 
       const { hits, totalHits } = await getPicturesByQuery(
         normalizedSearchQuery,
@@ -51,17 +37,22 @@ export const App = () => {
       );
 
       if (!totalHits) {
-        toast("We didn't find the pictures you are looking for :(");
+        setError("We didn't find the pictures you are looking for :(");
         return;
       }
       setPictures(prevPictures => [...prevPictures, ...hits]);
       setTotalImages(totalHits);
     } catch (error) {
-      setError(error);
+      setError('Something went wrong');
     } finally {
       setIsLoading(false);
     }
-  };
+  }, [page, perPage, searchQuery]);
+
+  useEffect(() => {
+    if (!searchQuery) return;
+    getPictures();
+  }, [page, searchQuery, getPictures]);
 
   const getQuery = ({ searchQuery }) => {
     if (prevSearchQueryRef.current === searchQuery) {
@@ -79,12 +70,11 @@ export const App = () => {
   };
 
   const handleLoadMoreClick = () => {
-    setPage(page + 1);
+    setPage(prevPage => prevPage + 1);
   };
 
   return (
     <StyledAppContainer>
-      <ToastContainer />
       <SearchBar onSubmit={getQuery} />
       <ImageGallery pictures={pictures} />
       {isLoading && <Loader count={perPage} />}
